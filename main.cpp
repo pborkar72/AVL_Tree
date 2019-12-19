61b5d25cda0bbe57aa33c0b30c482f939cc20515
pborkar72 <55620721+pborkar72@users.noreply.github.com>
2019-12-19 07:20:18 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include "avltree.h"
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    avltree tree;
+    vector<int> data;
+    //inserting sorted numbers
+    for(int i = 0; i < 1000; i++){
+        tree.insert(i);
+        data.push_back(i);
+        if(!tree.isBalanced()){
+            cout << "failed to balance after a insertion" << endl;
+            break;
+        }
+    }
+
+    //printing the tree in level order
+    tree.print();
+
+    //testing for balance
+    if(tree.isBalanced())
+        cout << "tree is balanced after all insertions" << endl;
+
+    //removing numbers randomly
+    for(int i : data){
+        tree.remove(i);
+        if(!tree.isBalanced()){
+            cout << "failed to balance after a deletion" << endl;
+            break;
+        }
+    }
+
+    //printing the tree in level order
+    tree.print();
+
+    //testing for balance
+    if(tree.isBalanced())
+        cout << "tree is balanced after all deletions" << endl;
+    else
+        cout << "tree is not balanced" << endl;
+
+}
